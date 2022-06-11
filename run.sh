bfd7e3e94f34634f061cc3aef5da0021eba289a7
amarees <saleh.amareen@dominos.com>
2022-06-11 16:16:08 -0400

    Added clean and run scripts

---
+++ run.sh
@@ -0,0 +1,9 @@
+
+
+
+bash clean.sh
+
+
+python SEA_LDA.py --maxtopic 19 --mintopic 12 --steptopic 1 --repetitions 5 --miniteration 1500 --maxiteration 1501 --stepiteration 500 --modelname "graphql"
+
+#python SEA_LDA.py --maxtopic 6 --mintopic 5 --steptopic 5 --repetitions 1 --miniteration 10 --maxiteration 11 --stepiteration 10 --modelname "graphql"
