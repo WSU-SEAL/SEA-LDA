dab0dd1ee07f1cafef83d9b5517cd56fc7ec6e34
amarees <saleh.amareen@dominos.com>
2024-01-09 14:20:42 -0500

    Added results for second-level mapping

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ bash clean.sh
 
 
 # Run GraphQL Posts
-python -Wignore SEA_LDA.py --maxtopic 16 --mintopic 5 --steptopic 1 --repetitions 10 --miniteration 1000 --maxiteration 1001 --stepiteration 5 --modelname "graphql" --sourcefileformat "xlsx"
+#python -Wignore SEA_LDA.py --maxtopic 16 --mintopic 5 --steptopic 1 --repetitions 10 --miniteration 1000 --maxiteration 1001 --stepiteration 5 --modelname "graphql" --sourcefileformat "xlsx"
 
 
 # Run Refactor Posts
-# python SEA_LDA.py --maxtopic 50 --mintopic 5 --steptopic 5 --repetitions 10 --miniteration 500 --maxiteration 2000 --stepiteration 500 --modelname "refactor"
+ python SEA_LDA.py --maxtopic 6 --mintopic 2 --steptopic 1 --repetitions 10 --miniteration 1000 --maxiteration 1001 --stepiteration 500 --modelname "query_mutation_topic"
 
 
 # Run IoT Posts
